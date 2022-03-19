a00c9acbc8dfd8bf56e3f7252a8897fd87d92174
Chayce Knaub <chknaub@yahoo.com>
2022-03-19 14:25:25 -0400

    adding forloop eamples

--- forloops/index.js
+++ forloops/index.js
@@ -1,1 +1,16 @@
 console.log("hello world");
+
+// some arrays to maybe use later in examples
+const numArr = [33, 34, -4, 5, 0, 3, 67, 345, 763, 13];
+
+const strArr = ["Chayce", "Angela", "Mark", "Joseph", "John"];
+
+// Write a function that adds up all numbers up to some number n
+//  example: n = 4
+// 1 + 2 + 3 + 4 = 10
+
+let addUpTo = (n) => {
+  let total = 0;
+
+  for (let i = 1; i <= n; i++) {}
+};
